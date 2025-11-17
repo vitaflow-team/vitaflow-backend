50cd8853eb726b908cc5f2c75f4c5bc2849ae82a
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-11-17 20:16:57 -0300

    chore(deps): add bcrypt for password hashing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module', 
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
