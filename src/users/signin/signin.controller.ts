a669d1566800021ea7382c95cfd194d84fc70249
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-12-31 11:49:54 -0300

    feat(clients): implement singinservice for business rule isolation

--- src/users/signin/signin.controller.ts
+++ src/users/signin/signin.controller.ts
@@ -1,27 +1,12 @@
-import { UserRepository } from '@/repositories/users/user.repository';
-import { AppError } from '@/utils/app.erro';
-import { PasswordHash } from '@/utils/password.hash';
-import { UploadService } from '@/utils/upload.service';
 import { Body, Controller, Post } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SignInDTO } from './signin.Dto';
-
-const UnauthorizedUser = 'Usuário não autorizado.';
-const AccountInactive = UnauthorizedUser + ' Conta inativa.';
+import { SignInService } from './signin.service';
 
 @ApiTags('User')
 @Controller('users')
 export class SignInController {
-  constructor(
-    private user: UserRepository,
-
-    private hash: PasswordHash,
-
-    private jwtService: JwtService,
-
-    private uploadService: UploadService,
-  ) {}
+  constructor(private service: SignInService) {}
 
   @ApiOperation({
     summary: 'User Sign-In',
@@ -53,50 +38,6 @@ export class SignInController {
   })
   @Post('signin')
   async postSignIn(@Body() body: SignInDTO) {
-    const { email, password, socialLogin } = body;
-
-    const user = await this.user.findByEmail({ email });
-    if (!user) {
-      throw new AppError(UnauthorizedUser, 401);
-    }
-
-    if (!user.active) {
-      throw new AppError(AccountInactive, 401);
-    }
-
-    if (!socialLogin) {
-      const validPassword = await this.hash.compareHash(
-        password,
-        user.password,
-      );
-      if (!validPassword) {
-        throw new AppError(UnauthorizedUser, 401);
-      }
-    }
-
-    const signedAvatarUrl = user.avatar
-      ? await this.uploadService.getSignedUrl(user.avatar)
-      : null;
-
-    const payload = {
-      name: user.name,
-      email: user.email,
-      productId: user.productId,
-      productType: user.product?.type,
-      productGroupId: user.product?.groupId,
-      avatar: signedAvatarUrl,
-      id: user.id,
-    };
-
-    return {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: signedAvatarUrl,
-      productId: user.productId,
-      productType: user.product?.type,
-      productGroupId: user.product?.groupId,
-      accessToken: await this.jwtService.signAsync(payload),
-    };
+    return this.service.postSignIn(body);
   }
 }
